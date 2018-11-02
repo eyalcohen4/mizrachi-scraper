e72da606311418f83b01e34adeb4d7223033de9f
Eyal Cohen <eyalcohen4.ec@gmail.com>
2018-11-02 13:25:06 +0200

    Initial

---
+++ mizrahi-scraper.js
@@ -0,0 +1,47 @@
+"use strict";
+
+require("dotenv").config({ path: __dirname + '../.env' });
+
+const pup = require("puppeteer");
+
+const MIZRACHI_LOGIN_URL =
+  "https://www.mizrahi-tefahot.co.il/he/bank/Pages/Default.aspx";
+const MIZRACHI_PASSWORD_SELECTOR = "#ctl00_PlaceHolderLogin_ctl00_tbPassword";
+const MIZRACHI_USERNAME_SELECTOR = "#ctl00_PlaceHolderLogin_ctl00_tbUserName";
+const MIZRACHI_USERNAME = process.env.USERNAME;
+const MIZRACHI_PASSWORD = process.env.PASSWORD;
+
+const getMizrachiBalance = async () => {
+  try {
+    const browser = await pup.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      headless: false
+    });
+    const page = await browser.newPage();
+
+    await page.goto(MIZRACHI_LOGIN_URL);
+    await page.type(MIZRACHI_USERNAME_SELECTOR, MIZRACHI_USERNAME);
+    await page.type(MIZRACHI_PASSWORD_SELECTOR, MIZRACHI_PASSWORD);
+    await page.click("#ctl00_PlaceHolderLogin_ctl00_Enter");
+
+    await page.waitForSelector(".sky-account-balance", {
+      timeout: 500 * 1000
+    });
+
+    const balance = await page.evaluate(() =>
+      document
+        .querySelector(".sky-big3 .green")
+        .getAttribute("miz-numeric-colorup")
+    );
+
+    console.log(`===== balance ${balance} =====`);
+
+    await browser.close();
+
+    return balance;
+  } catch (error) {
+    console.error(`mizrachi error ${error}`);
+  }
+};
+
+await getMizrachiBalance();
